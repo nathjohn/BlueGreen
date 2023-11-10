9926a788be8f560dcc3290ccf36898183adb57a3
Mikkel Bach Nielsen <103189707+mbn-ms-dk@users.noreply.github.com>
2023-11-10 11:27:03 +0100

    Deleted unnecessary files and configurations. (#30)

---
+++ test/BgApp.Tests/CounterTests.cs
@@ -0,0 +1,20 @@
+namespace BgApp.Tests;
+
+public class CounterTests
+{
+    [Fact]
+    public void CounterShouldIncrementWhenClicked()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var cut = ctx.RenderComponent<Counter>();
+        var paraElm = cut.Find("p");
+
+        // Act
+        cut.Find("button").Click();
+
+        // Assert
+        var paraElmText = paraElm.TextContent;
+        paraElmText.MarkupMatches("Current count: 1");
+    }
+}
