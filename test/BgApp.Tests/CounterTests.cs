bf42ab6aa69360739ebc2f964601f7c5ef0852e7
Mikkel Bach Nielsen <103189707+mbn-ms-dk@users.noreply.github.com>
2023-11-28 15:55:33 +0100

    update comments (#46)

--- test/BgApp.Tests/CounterTests.cs
+++ test/BgApp.Tests/CounterTests.cs
@@ -8,15 +8,15 @@ public class CounterTests
     [Fact]
     public void CounterShouldIncrementWhenClicked()
     {
-        // Arrange
+        // Arrange - Find the counter component
         using var ctx = new TestContext();
         var cut = ctx.RenderComponent<Counter>();
         var paraElm = cut.Find("p");
 
-        // Act
+        // Act - Click the button
         cut.Find("button").Click();
 
-        // Assert
+        // Assert - Check that the counter was incremented
         var paraElmText = paraElm.TextContent;
         paraElmText.MarkupMatches("Current count: 1");
     }
